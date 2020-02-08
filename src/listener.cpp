c3d41a055094f849445618df2647f92d223953ff
mananmadan <madan.manan1@gmail.com>
2020-02-09 01:45:08 +0530

    C++ libraries compiled ,now priniting map data and position using cout<<

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,15 +1,27 @@
 #include "ros/ros.h"
+#include "bits/stdc++.h"//c++ header now compiling
 #include "std_msgs/String.h"
 #include "std_msgs/Header.h"
 #include "nav_msgs/OccupancyGrid.h"
+#define pb push_back
 using namespace std_msgs;
+using namespace std;
 using namespace nav_msgs;
+vector<int> mytest;
 void mapcallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
  std_msgs::Header header  = msg->header;
  nav_msgs::MapMetaData info = msg->info;
- ROS_INFO("got map %d %d",info.width,info.height);
- ROS_INFO("%d",info.origin.position.x);
+// int8_t data[];
+ ROS_INFO(" ");//this is nessecarry before any cout in order to produce the see the output on the log;
+cout<<"-------------------------------Printing map data -------------------------------"<<"\n";
+for(auto it : msg->data)
+cout<<(int)it<<" ";
+cout<<"-------------------------------Printed all map data ----------------------------"<<"\n";
+cout<<"---------------------------------Printing position------------------------------"<<"\n";
+cout<<"i am here"<<info.origin.position.x<<" "<<info.origin.position.y;
+//cout<<"--------------------------------------Printing velocity-------------------------"<<"\n";
+//for doing this subscribe to the sensor fusion data /odom/filetered or /imu/data 
 }
 
 int main(int argc, char **argv)
