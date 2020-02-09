572b75c6b3bfa2ec2e2787278d514f6b6d2ccfc9
Nipunnyka <nipunika.co18@gmail.com>
2020-02-09 17:14:11 +0530

    first

--- src/listener.cpp
+++ src/listener.cpp
@@ -3,6 +3,7 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Header.h"
 #include "nav_msgs/OccupancyGrid.h"
+#include<geometry_msgs/Twist.h> //for linear velocity
 #define pb push_back
 using namespace std_msgs;
 using namespace std;
@@ -20,8 +21,21 @@ cout<<(int)it<<" ";
 cout<<"-------------------------------Printed all map data ----------------------------"<<"\n";
 cout<<"---------------------------------Printing position------------------------------"<<"\n";
 cout<<"i am here"<<info.origin.position.x<<" "<<info.origin.position.y;
-//cout<<"--------------------------------------Printing velocity-------------------------"<<"\n";
-//for doing this subscribe to the sensor fusion data /odom/filetered or /imu/data 
+
+}
+
+void velCallBack(const geometry_msgs::Twist& msg){
+    float linx, liny;
+    ROS_INFO("");
+    cout<<"--------------------------------------Printing velocity-------------------------"<<"\n";
+    //for doing this subscribe to the sensor fusion data /odom/filetered or /imu/data 
+    //use this topic /cmd_vel_mux/input/navi to move the robot
+    //subcribe to /odom for linear vel
+    linx=msg.linear.x;
+    liny=msg.linear.y;
+    cout<<"linear velocity in x:"<<linx<<endl;
+    cout<<"linear velocity in y:"<<liny<<endl;
+
 }
 
 int main(int argc, char **argv)
@@ -29,6 +43,7 @@ int main(int argc, char **argv)
  ros::init(argc,argv,"listener");
  ros::NodeHandle n;
  ros::Subscriber map_sub = n.subscribe("/move_base/local_costmap/costmap",1000,mapcallback);
+ ros::Subscriber vel_sub = n.subscribe("/odom", 1000, velCallBack);
 ros::spin();
 return 0;
 }
