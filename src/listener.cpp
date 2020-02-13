961ebcfa811801e04d05dcdd5247a931cc545a3b
mananmadan <madan.manan1@gmail.com>
2020-02-13 18:20:06 +0530

    test to see map sizes done

--- src/listener.cpp
+++ src/listener.cpp
@@ -10,19 +10,25 @@ using namespace std_msgs;
 using namespace std;
 using namespace nav_msgs;
 vector<int> mytest;
+vector<int> data_sizes;
 void mapcallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
  std_msgs::Header header  = msg->header;
  nav_msgs::MapMetaData info = msg->info;
 // int8_t data[];
  ROS_INFO(" ");//this is nessecarry before any cout in order to produce the see the output on the log;
-cout<<"-------------------------------Printing map data -------------------------------"<<"\n";
-//for(auto it : msg->data)
-//cout<<(int)it<<" ";
-cout<<"-------------------------------Printed all map data ----------------------------"<<"\n";
+//cout<<"-------------------------------Printing map data -------------------------------"<<"\n";
+for(auto it : msg->data)//is this printing the data 60 then 60 then 60 ---> so on 
+mytest.pb((int)it);
+//cout<<"-------------------------------Printed all map data ----------------------------"<<"\n";
+cout<<"array size"<<mytest.size()<<"\n";//writing it to check my data
+data_sizes.pb(mytest.size());
+//cout<<"my map data should be of"<<info.width<<info.height<<"\n";//this is printing 60 and 60 
 cout<<"---------------------------------Printing position------------------------------"<<"\n";
 cout<<"i am here"<<info.origin.position.x<<" "<<info.origin.position.y;
-
+cout<<"-------------------------------All map data sizes-------------------------------"<<"\n";
+for(auto it : data_sizes)
+cout<<(int)it<<"\n";
 }
 
 void velCallBack(const nav_msgs::Odometry& msg){
@@ -44,7 +50,7 @@ int main(int argc, char **argv)
  ros::init(argc,argv,"listener");
  ros::NodeHandle n; 
  ros::Subscriber map_sub = n.subscribe("/move_base/local_costmap/costmap",1000,mapcallback);
- ros::Subscriber vel_sub = n.subscribe("/odom", 1000, velCallBack);
+ //ros::Subscriber vel_sub = n.subscribe("/odom", 1000, velCallBack);//comment out to do actual node programming
   cout<<"vel_sub"<<"\n";
 ros::spin();
 return 0;
